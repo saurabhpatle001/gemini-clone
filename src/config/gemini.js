1a458e5b20cb8b5fc80c3fb24d017a4d8cfaeb9d
saurabhpatle001 <surabhpatle001@gmail.com>
2024-11-17 14:01:33 +0530

    first commit

---
+++ src/config/gemini.js
@@ -0,0 +1,56 @@
+/** Install the Generative AI SDK*
+ * $ npm install @google/generative-ai*/
+
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
+
+const MODEL_NAME = "gemini-1.0-pro";
+const API_KEY = "AIzaSyBS4G2uPM6l8NQ825CuKmXGvX6OlQ3DLX0";
+
+async function run(prompt) {
+  const genAI = new GoogleGenerativeAI(API_KEY);
+  const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+
+  const generationConfig = {
+    temperature: 0.9,
+    topP: 1,
+    topK: 1,
+    maxOutputTokens: 2048,
+  };
+
+  const safetySettings = [ 
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+  ];
+
+  const chat = model.startChat({
+    generationConfig,
+    safetySettings,
+    history: [
+    ],
+  });
+
+  const result = await chat.sendMessage(prompt);
+  const response = result.response;
+  console.log(await response.text());
+  return response.text();
+}
+
+export default run;
