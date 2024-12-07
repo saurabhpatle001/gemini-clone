42caf9933ee0ab94c8c4dba40e6c7ce94f94da58
saurabhpatle001 <surabhpatle001@gmail.com>
2024-12-07 11:56:36 +0530

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/gemini-clone",
+  base: "/gemini-clone/",
   server: {
     port:3000
   }
